d5ba35cb6aef81591b040a9c20beaa3a60bfc963
Maxim Danilov <abl1v1on@yandex.ru>
2024-05-22 04:11:12 +0500

    deleted old urls

--- anime/urls.py
+++ anime/urls.py
@@ -5,12 +5,16 @@ from . import views
 
 app_name = 'anime'
 
-router = SimpleRouter()
-router.register(r'anime', views.AnimeViewSet)
+anime_router = SimpleRouter()
+anime_router.register(r'anime', views.AnimeViewSet)
 
+# like_router = SimpleRouter()
+# like_router.register(r'likes', views.LikeViewSet)
 
 urlpatterns = [
-    
+    # path('like/', views.LikeCreateAPIView.as_view(), name='like'),
+    # path('like-delete/', views.DeleteLikeAPIView.lookup_field, name='like_delete'),
 ]
 
-urlpatterns += router.urls
+urlpatterns += anime_router.urls
+# urlpatterns += like_router.urls
