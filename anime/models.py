57291f14f126530fc0d214ecdfa94e09e978e040
Maxim Danilov <abl1v1on@yandex.ru>
2024-05-22 04:10:45 +0500

    created total_likes method and changed likes field

--- anime/models.py
+++ anime/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.contrib.contenttypes.fields import GenericRelation
 
 
 ANIME_TYPE_CHOISES = [
@@ -32,6 +33,7 @@ class Anime(models.Model):
     duration = models.CharField('Продолжительность', max_length=50)
     views = models.PositiveIntegerField('Просмотры', blank=True, default=0)
     cover = models.ImageField('Обложка', upload_to='anime_covers/%Y/%m', blank=True)
+    likes = models.ManyToManyField(get_user_model(), related_name='anime_likes', blank=True, default=0)
     slug = models.SlugField('URL', max_length=255, db_index=True, unique=True)
 
     class Meta:
@@ -40,6 +42,9 @@ class Anime(models.Model):
     
     def __str__(self) -> str:
         return self.title
+
+    def total_likes(self):
+        return self.likes.count()
 
 
 class Studio(models.Model):
@@ -85,4 +90,17 @@ class ViewingStatus(models.Model):
     
     def __str__(self) -> str:
         return self.name
-    
+
+
+# class Like(models.Model):
+#     user = models.ForeignKey(
+#         get_user_model(), 
+#         on_delete=models.CASCADE,
+#         verbose_name='Пользователь'
+#     )
+#     anime = models.ForeignKey(
+#         Anime,
+#         on_delete=models.CASCADE,
+#         verbose_name='Аниме',
+#         related_name='likes_anime'
+#     )
