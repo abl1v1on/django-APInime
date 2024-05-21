d273adc698e849d22de7360af3bf1b351308a2c2
Maxim Danilov <abl1v1on@yandex.ru>
2024-05-22 04:11:23 +0500

    deleted old views

--- anime/views.py
+++ anime/views.py
@@ -1,6 +1,8 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
+from rest_framework.generics import CreateAPIView
 from rest_framework.filters import OrderingFilter
-from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from django_filters.rest_framework import DjangoFilterBackend
 
 from .serializers import AnimeSerializer
@@ -14,5 +16,23 @@ class AnimeViewSet(ModelViewSet):
     serializer_class = AnimeSerializer
     filter_backends = [DjangoFilterBackend, OrderingFilter]
     filterset_class = AnimeFilter
-    ordering_fields = ['date_aired', 'title']    
+    ordering_fields = ['date_aired', 'title', 'likes']    
     permission_classes = [IsAdminUserOrReadOnly]
+
+
+# class LikeViewSet(ModelViewSet):
+#     queryset = Like.objects.all()
+#     serializer_class = LikeSerializer
+#     filter_backends = [DjangoFilterBackend]
+#     filterset_class = LikeFilter
+#     # ordering_fields = ['date_aired', 'title']    
+#     permission_classes = [IsAuthenticatedOrReadOnly]
+
+#     def create(self, request, *args, **kwargs):
+#         if request.data:
+#             if Like.objects.filter(
+#                 user_id=request.data['user'], 
+#                 anime_id=request.data['anime']
+#             ).exists():
+#                 return Response({'detail': 'It\'s already been liked.'})
+#         return super().create(request, *args, **kwargs)
