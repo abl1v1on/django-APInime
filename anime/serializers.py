0ebc3fc01ad3ca955b2f2572f5ad938b707353ca
Maxim Danilov <abl1v1on@yandex.ru>
2024-05-22 04:11:02 +0500

    created like serializer

--- anime/serializers.py
+++ anime/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import Anime, Studio, Genre
+from .models import Anime, Studio
 
 class StudioSerializer(serializers.ModelSerializer):
     class Meta:
@@ -12,7 +12,19 @@ class AnimeSerializer(serializers.ModelSerializer):
     rating = serializers.FloatField(read_only=True)
     views = serializers.IntegerField(read_only=True)
     studio = StudioSerializer()
+    likes = serializers.SerializerMethodField()
 
     class Meta:
         model = Anime
         fields = '__all__'
+    
+    def get_likes(self, obj: Anime):
+        print(Anime.likes)
+        print('\n\n\n')
+        return obj.total_likes()
+
+
+class LikeSerializer(serializers.Serializer):
+    user = serializers.IntegerField()
+    anime = serializers.IntegerField()
+
