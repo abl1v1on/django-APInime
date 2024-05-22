d272aeb5521dd0731c12049ee4743cdd083008a7
Maxim Danilov <abl1v1on@yandex.ru>
2024-05-22 09:04:32 +0500

    created like view set

--- anime/views.py
+++ anime/views.py
@@ -1,14 +1,15 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
-from rest_framework.generics import CreateAPIView
+from rest_framework.decorators import api_view
 from rest_framework.filters import OrderingFilter
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from django_filters.rest_framework import DjangoFilterBackend
 
-from .serializers import AnimeSerializer
-from .filters import AnimeFilter
+from .serializers import AnimeSerializer, LikeSerializer
+from .filters import AnimeFilter, LikeFilter
 from .utils import utils
 from .permissions import IsAdminUserOrReadOnly
+from .models import Anime, Like
 
 
 class AnimeViewSet(ModelViewSet):
@@ -16,23 +17,22 @@ class AnimeViewSet(ModelViewSet):
     serializer_class = AnimeSerializer
     filter_backends = [DjangoFilterBackend, OrderingFilter]
     filterset_class = AnimeFilter
-    ordering_fields = ['date_aired', 'title', 'likes']    
+    ordering_fields = ['date_aired', 'title']    
     permission_classes = [IsAdminUserOrReadOnly]
 
 
-# class LikeViewSet(ModelViewSet):
-#     queryset = Like.objects.all()
-#     serializer_class = LikeSerializer
-#     filter_backends = [DjangoFilterBackend]
-#     filterset_class = LikeFilter
-#     # ordering_fields = ['date_aired', 'title']    
-#     permission_classes = [IsAuthenticatedOrReadOnly]
+class LikeViewSet(ModelViewSet):
+    queryset = Like.objects.all()
+    serializer_class = LikeSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = LikeFilter   
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
-#     def create(self, request, *args, **kwargs):
-#         if request.data:
-#             if Like.objects.filter(
-#                 user_id=request.data['user'], 
-#                 anime_id=request.data['anime']
-#             ).exists():
-#                 return Response({'detail': 'It\'s already been liked.'})
-#         return super().create(request, *args, **kwargs)
+    def create(self, request, *args, **kwargs):
+        if request.data:
+            if Like.objects.filter(
+                user_id=request.data['user'], 
+                anime_id=request.data['anime']
+            ).exists():
+                return Response({'detail': 'It\'s already been liked.'})
+        return super().create(request, *args, **kwargs)
