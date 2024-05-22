7da4e85e4389e37e740b48004e264838d1d33da5
Maxim Danilov <abl1v1on@yandex.ru>
2024-05-22 16:34:25 +0500

    creaetd anime series serializer

--- anime/serializers.py
+++ anime/serializers.py
@@ -1,6 +1,7 @@
+from django.forms import model_to_dict
 from rest_framework import serializers
 
-from .models import Anime, Studio, Like
+from .models import Anime, Studio, Like, AnimeSeries
 
 class StudioSerializer(serializers.ModelSerializer):
     class Meta:
@@ -8,19 +9,24 @@ class StudioSerializer(serializers.ModelSerializer):
         fields = ['id', 'slug']
 
 
+class AnimeSeriesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AnimeSeries
+        fields = '__all__'
+
+
 class AnimeSerializer(serializers.ModelSerializer):
     rating = serializers.FloatField(read_only=True)
     views = serializers.IntegerField(read_only=True)
     studio = StudioSerializer()
     likes = serializers.SerializerMethodField()
-
+    
     class Meta:
         model = Anime
         fields = '__all__'
     
     def get_likes(self, obj: Anime):
         return obj.total_likes()
-
 
 class LikeSerializer(serializers.ModelSerializer):
     class Meta:
