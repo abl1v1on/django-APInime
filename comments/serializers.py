8b855b5696942bee61abc2c007b124920df6e026
Maxim Danilov <abl1v1on@yandex.ru>
2024-05-24 04:48:42 +0500

    changed comment serialize

--- comments/serializers.py
+++ comments/serializers.py
@@ -4,6 +4,10 @@ from .models import Comment
 
 
 class CommentSerializer(serializers.ModelSerializer):
+    user = serializers.CharField(
+        read_only=True
+    )
+    
     class Meta:
         model = Comment
         fields = '__all__'
