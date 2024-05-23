cd30fba78269032d7897455277edc1718c64ce81
Maxim Danilov <abl1v1on@yandex.ru>
2024-05-24 04:48:24 +0500

    added destroy and perfom_create methods fot like view set

--- anime/views.py
+++ anime/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import get_object_or_404
+from rest_framework import status
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
-from rest_framework.views import APIView
 from rest_framework.filters import OrderingFilter
+from rest_framework.exceptions import ValidationError
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 
 from .serializers import AnimeSerializer, LikeSerializer, AnimeSeriesSerializer
@@ -11,6 +12,7 @@ from .filters import AnimeFilter, LikeFilter, AnimeSeriesFilter
 from .utils import utils
 from .permissions import IsAdminUserOrReadOnly
 from .models import Anime, Like, AnimeSeries
+from comments.utils import is_author
 
 
 class AnimeViewSet(ModelViewSet):
@@ -37,11 +39,15 @@ class LikeViewSet(ModelViewSet):
     filterset_class = LikeFilter   
     permission_classes = [IsAuthenticatedOrReadOnly]
 
-    def create(self, request, *args, **kwargs):
-        if request.data:
-            if Like.objects.filter(
-                user_id=request.data['user'], 
-                anime_id=request.data['anime']
-            ).exists():
-                return Response({'detail': 'It\'s already been liked.'})
-        return super().create(request, *args, **kwargs)
+    def destroy(self, request, *args, **kwargs):
+        instance = is_author(self.get_object(), request)
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def perform_create(self, serializer):
+        anime_id = self.request.data.get('anime')
+        anime = get_object_or_404(Anime, pk=anime_id)
+        if Like.objects.filter(user=self.request.user, anime=anime).exists():
+            raise ValidationError(detail='You have already liked this anime')
+        serializer.validated_data['user'] = self.request.user
+        serializer.save()
