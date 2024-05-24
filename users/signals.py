db9a68fc220c7a4c854d6282073b798527d62c52
Maxim Danilov <abl1v1on@yandex.ru>
2024-05-24 08:30:52 +0500

    created signal

--- users/signals.py
+++ users/signals.py
@@ -1,7 +1,22 @@
 from django.contrib.auth import get_user_model
 from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 from .models import Profile
+from anime.utils import call_in_new_anime_episodes
+
+
+
+@receiver(post_save, sender=get_user_model())
+def send_notification_on_new_episode(sender, instance, created, **kwargs):
+    if created:
+        if instance.is_subscribed:
+            call_in_new_anime_episodes(
+                'Добро пожаловать!',
+                'Вы подписались на рассылку',
+                [instance.email],
+                html_template='anime/welcome_email_message.html'
+            )
 
 
 def create_user_profile(sender, instance, created, **kwargs):
