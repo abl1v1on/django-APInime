f8b2d1a6bf1e86b8789b7b5e240d29f60a5d30cf
Maxim Danilov <abl1v1on@yandex.ru>
2024-05-24 06:19:20 +0500

    created ready method

--- anime/apps.py
+++ anime/apps.py
@@ -5,3 +5,6 @@ class AnimeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'anime'
     verbose_name = 'Аниме'
+
+    def ready(self):
+        import anime.signals
