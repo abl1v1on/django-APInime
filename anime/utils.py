ecc35b957f424d71a4fbb56484151c4ce4854ff4
Maxim Danilov <abl1v1on@yandex.ru>
2024-05-24 05:25:08 +0500

    changed utils for anime app

--- anime/utils.py
+++ anime/utils.py
@@ -1,12 +1,17 @@
-from .models import Anime, models
+from .models import Anime, AnimeSeries, Like, models
 
 
-class AnimeUtils:
+class Utils:
     def __init__(self, model: models.Model) -> None:
         self.model = model
 
-    def get_anime(self):
+    def get_objects(self):
         return self.model.objects.all()
+    
+    def is_exist(self, **kwargs):
+        return self.model.objects.filter(**kwargs).exists()
 
 
-utils = AnimeUtils(model=Anime)
+anime_utils = Utils(model=Anime)
+anime_series_urils = Utils(model=AnimeSeries)
+likes_utils = Utils(model=Like)
