e295cd5bd02d4933849b2ab99d3f67fcb6fe0b83
Maxim Danilov <abl1v1on@yandex.ru>
2024-05-25 04:10:33 +0400

    created profile view set router

--- users/urls.py
+++ users/urls.py
@@ -1,14 +1,20 @@
 from django.urls import path 
 
 from rest_framework_simplejwt.views import TokenRefreshView
+from rest_framework.routers import SimpleRouter
 
-from .views import CreateUserView, CustomTokenObtainPairView
+from .views import CreateUserView, CustomTokenObtainPairView, ProfileViewSet
+
 
 app_name = 'user'
 
+router = SimpleRouter()
+router.register(r'profiles', ProfileViewSet)
 
 urlpatterns = [
     path('token/', CustomTokenObtainPairView.as_view(), name='token'),
     path('token/refresh/', TokenRefreshView.as_view(), name='refresh_token'),
     path('signup/', CreateUserView.as_view(), name='signup'),
 ]
+
+urlpatterns += router.urls
