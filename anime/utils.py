33675a5fdb4b4ec0046cc290da3813b0f9c371d4
Maxim Danilov <abl1v1on@yandex.ru>
2024-05-26 05:49:01 +0500

    added methods fot get objects from models

--- anime/utils.py
+++ anime/utils.py
@@ -1,8 +1,9 @@
 from config import settings
 from django.core.mail import EmailMultiAlternatives
+from django.db.models import Count, Case, When, Prefetch
 from django.template.loader import render_to_string
 
-from .models import Anime, AnimeSeries, Like, models
+from .models import Anime, AnimeSeries, Like, models, Genre
 
 
 class Utils:
@@ -33,6 +34,16 @@ def call_in_new_anime_episodes(
     html_content = render_to_string(html_template, context)
     msg.attach_alternative(html_content, 'text/html')
     msg.send()
+
+
+def get_all_anime():
+    return Anime.objects.all().annotate(
+            likes_count=Count(Case(When(likes_anime__like=True, then=1))),    
+        ).prefetch_related('genres', 'studio')
+
+
+def get_likes():
+    return Like.objects.prefetch_related('user', 'anime').all()
 
 
 anime_utils = Utils(model=Anime)
