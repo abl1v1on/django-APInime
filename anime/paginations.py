eac545ab8eb0eebb10c518cd9fa67acd096e1e26
Maxim Danilov <abl1v1on@yandex.ru>
2024-05-26 07:40:52 +0500

    created pagination class for anime view set

---
+++ anime/paginations.py
@@ -0,0 +1,8 @@
+from rest_framework.pagination import PageNumberPagination
+
+
+class AnimeListAPIViewPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 50
+    
